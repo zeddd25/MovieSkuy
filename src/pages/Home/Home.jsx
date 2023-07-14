1a26051e0397a7bd4299f4fe2277592f84bb75bc
Mohammad Dzilli <muhamaddzilliassajad@gmail.com>
2023-07-14 14:30:28 +0700

    update home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,43 +1,18 @@
-
 import { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
 import "../Home/Home.css"
-import Navbar from "../../components/Navbar/Navbar"
 import MovieList from "../../components/MovieList/MovieList"
-import TvShowList from "../../components/TvShowList/TvShowList"
+import Navbar from "../../components/Navbar/Navbar"
+import HeroBanner from "../Home/HeroBanner/HeroBanner"
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <MovieList />
-import { useEffect, useState } from "react";
-
-import { NavLink } from "react-router-dom";
-import "../Home/Home.css";
-import MovieList from "../../components/Movielist/MovieList";
-// Component
-import Navbar from "../../components/Navbar/Navbar";
-import HeroBanner from "./HeroBanner/HeroBanner";
-
-const Home = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=a7a276360f82d61084bcdb311cb45b7e"
-    )
-      .then((res) => res.json())
-      .then((data) => setPopularMovies(data.results));
-  }, []);
 
   return (
     <div>
       <Navbar />
       <HeroBanner />
-      <MovieList />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
