8172a0a7ab97e7acde71970bbc1d8047f1059da4
Mohammad Dzilli <muhamaddzilliassajad@gmail.com>
2023-07-15 14:51:41 +0700

    update fitur/movielist

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -7,41 +7,61 @@ import Navbar from "../Navbar/Navbar";
 
 const MovieList = () => {
   const [movieList, setMovieList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   const { type } = useParams();
 
   useEffect(() => {
+    setMovieList([]);
+    setCurrentPage(1);
+  }, [type]);
+
+
+  useEffect(() => {
     const fetchData = async () => {
-      const movies = await getMovieList(type || "popular");
-      setMovieList(movies);
+      const movieData = await getMovieList(type || "popular", currentPage);
+      setMovieList((prevList) => [...prevList, ...movieData.results]);
+      setTotalPages(movieData.total_pages);
     };
 
     fetchData();
-  }, [type]);
+  }, [type, currentPage]);
 
   const imageBaseUrl = import.meta.env.VITE_APP_BASE_IMAGE_URL;
 
+  const handleLoadMore = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
   return (
-    <>
-    <Navbar />  
-        <div className="movie_list">
-          {/* <h2 className="list_title">
-            {(type ? type : "POPULAR").toUpperCase()}
-          </h2> */}
-          <div className="list_cards">
-            {movieList.map((movie, i) => (
-              <Card
-                key={i}
-                movie={movie}
-                movieTitle={movie?.original_title || ""}
-                movieRelese={movie?.release_date || ""}
-                movieRating={movie?.vote_average || ""}
-                movieDescription={movie?.overview?.slice(0, 118) + "..." || ""}
-                movieImage={`${imageBaseUrl}${movie?.poster_path || ""}`}
-              />
-            ))}
-          </div>
+    <div className="container">
+      <Navbar />
+      <div className="movie_list">
+        <h2 className="list_title">{type ? type.toUpperCase() : "POPULAR"}</h2>
+        <div className="list_cards">
+          {movieList.map((movie, i) => (
+            <Card
+              key={i}
+              movie={movie}
+              movieTitle={movie?.original_title || ""}
+              movieRelese={movie?.release_date || ""}
+              movieRating={movie?.vote_average || ""}
+              movieDescription={movie?.overview?.slice(0, 118) + "..." || ""}
+              movieImage={`${imageBaseUrl}${movie?.poster_path || ""}`}
+            />
+          ))}
         </div>
-    </>
+        <div className="button_container">
+        {currentPage < totalPages && (
+          <button onClick={handleLoadMore} className="load_more_button">
+            Load More
+          </button>
+        )}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,65 @@
-import React from "react";
 import "../Navbar/Navbar.css";
 import { NavLink } from "react-router-dom";
 
+// komponent dropdown list untuk navbar
+const DropdownList = ({ items }) => {
+  return (
+    <div className="dropdown_list">
+      <span>
+        {items.map((item) => (
+          <NavLink to={item.url} className="text_dropdown" key={item.id}>
+            {item.icon} {item.label}
+          </NavLink>
+        ))}
+      </span>
+    </div>
+  );
+};
+
+// komponent navbar
 const Navbar = () => {
+  const movieItems = [
+    { id: 1, url: "/movies/top_rated", label: "Top Rated", icon: "🎥" },
+    { id: 2, url: "/movies/popular", label: "Popular", icon: "🎥" },
+    { id: 3, url: "/movies/upcoming", label: "Upcoming", icon: "🎥" },
+    { id: 4, url: "/movies/now_playing", label: "Now Playing", icon: "🎥" },
+  ];
+
+  const tvShowItems = [
+    { id: 1, url: "/tvshow/airing_today", label: "Airing Today" },
+    { id: 2, url: "/tvshow/on_the_air", label: "On the Air" },
+    { id: 3, url: "/tvshow/popular", label: "Popular" },
+    { id: 4, url: "/tvshow/top_rated", label: "Top Rated" },
+  ];
+
+  const trendingItems = [
+    { id: 1, url: "/movies/now_playing", label: "Now Playing" },
+    { id: 2, url: "/movies/popular", label: "Popular" },
+    { id: 3, url: "/movies/top_rated", label: "Top Rated" },
+    { id: 4, url: "/movies/upcoming", label: "Upcoming" },
+  ];
+
   return (
     <div className="container">
-    <div className="navbar">
-      <NavLink to={"/"} className="logo">
-        <img src="../src/assets/images/MovieSkuys.png" alt="logo" />
-      </NavLink>
-      <ul>
-        <li className="movie-list">
-          Movie List
-          <div className="dropdown_list">
-          <span id="font">
-          <NavLink to="/movies/now_playing" className="text_dropdown">Now Playing</NavLink>
-          <NavLink to="/movies/popular" className="text_dropdown">Popular</NavLink>
-          <NavLink to="/movies/top_rated" className="text_dropdown">Top Rated</NavLink>
-          <NavLink to="/movies/upcoming" className="text_dropdown">Upcoming</NavLink>
-          </span>
+      <div className="navbar">
+        <NavLink to={"/"} className="logo">
+          <img src="../src/assets/images/MovieSkuys.png" alt="logo" />
+        </NavLink>
+        <ul>
+          <div className="movie-list">
+            Movie List
+            <DropdownList items={movieItems} />
           </div>
-        </li>
-        <li className="movie-list">
-        TV Show
-          <div className="dropdown_list">
-          <span id="font">
-          <NavLink to="/tvshow/airing_today" className="text_dropdown">airing today</NavLink>
-          <NavLink to="/tvshow/on_the_air" className="text_dropdown">on the air</NavLink>
-          <NavLink to="/tvshow/popular" className="text_dropdown">popular</NavLink>
-          <NavLink to="/tvshow/top_rated" className="text_dropdown">top rated</NavLink>
-          </span>
-          </div>
-        </li>
-        <li className="movie-list">
-        Trending
-          <div className="dropdown_list">
-          <span id="font">
-          <NavLink to="/movies/now_playing" className="text_dropdown">Now Playing</NavLink>
-          <NavLink to="/movies/popular" className="text_dropdown">Popular</NavLink>
-          <NavLink to="/movies/top_rated" className="text_dropdown">Top Rated</NavLink>
-          <NavLink to="/movies/upcoming" className="text_dropdown">Upcoming</NavLink>
-          </span>
-          </div>
-        </li>
-        {/* <li>TV Show</li> */}
-        {/* <li>Trending</li> */}
-      </ul>
-    </div>
+          <li className="movie-list">
+            TV Show
+            <DropdownList items={tvShowItems} />
+          </li>
+          <li className="movie-list">
+            Trending
+            <DropdownList items={trendingItems} />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
