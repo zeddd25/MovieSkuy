b2ddfdbbd47d220bf9b46dba39c9d7f92b5c9ac6
im4soft02 <ahmad.adhin.02@gmail.com>
2023-07-15 15:07:38 +0700

    benerin?error

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,9 +1,6 @@
 const apiKey = import.meta.env.VITE_APP_API_KEY;
 const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
-export const getMovieList = async (type) => {
-  const movieResponse = await fetch(
-    `${baseUrl}/movie/${type}?api_key=${apiKey}&page=30`
 export const getMovieList = async (type, page) => {
   const movieResponse = await fetch(
     `${baseUrl}/movie/${type}?api_key=${apiKey}&page=${page}`
@@ -13,14 +10,9 @@ export const getMovieList = async (type, page) => {
   return movieData;
 };
 
-export const getTvShowList = async (type) => {
-  const tvshowResponse = await fetch(
-    `${baseUrl}/tv/${type}?api_key=${apiKey}&page=5`
-
 export const getTvShowList = async (type, page) => {
   const tvshowResponse = await fetch(
     `${baseUrl}/tv/${type}?api_key=${apiKey}&page=${page}`
-
   );
   const tvshowData = await tvshowResponse.json();
   return tvshowData;
