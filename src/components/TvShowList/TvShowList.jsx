8774dc931de5bc2cbbc1c7be5a95860ad9471f16
Mohammad Dzilli <muhamaddzilliassajad@gmail.com>
2023-07-18 09:31:39 +0700

    update/tvhowlist

--- src/components/TvShowList/TvShowList.jsx
+++ src/components/TvShowList/TvShowList.jsx
@@ -1,47 +1,67 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "../TvShowList/TvShowList.css"
+import "../TvShowList/TvShowList.css";
 import Card from "../Card/Card";
 import { getTvShowList } from "../../Api/Api";
 import Navbar from "../Navbar/Navbar";
 
 const TvShowList = () => {
   const [tvShowList, setTvShowList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   const { type } = useParams();
 
   useEffect(() => {
+    setTvShowList([]);
+    setCurrentPage(1);
+  }, [type]);
+
+  useEffect(() => {
     const fetchData = async () => {
-      const tvshow = await getTvShowList(type || "popular");
-      setTvShowList(tvshow);
+      const tvShowData = await getTvShowList(type || "popular", currentPage);
+      setTvShowList((prevList) => [...prevList, ...tvShowData.results]);
+      setTotalPages(tvShowData.total_pages);
     };
 
     fetchData();
-  }, [type]);
+  }, [type, currentPage]);
 
   const image = import.meta.env.VITE_APP_BASE_IMAGE_URL;
 
+  const handleLoadMore = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+
   return (
-    <>
+    <div className="container">
       <Navbar />
-      <div className="movie_list">
-        <h2 className="list_title">
-          {(type ? type : "POPULAR").toUpperCase()}
-        </h2>
+      <div className="tvshow_list">
+        <h2 className="list_title">{type ? type.toUpperCase() : "POPULAR"}</h2>
         <div className="list_cards">
           {tvShowList.map((tvshow, i) => (
             <Card
               key={i}
               movie={tvshow}
-              movieTitle={tvshow?tvshow.original_name:""}
-              movieRelese={tvshow?tvshow.release_date:""}
-              movieRating={tvshow?tvshow.vote_average:""}
-              movieDescription={tvshow ? tvshow.overview.slice(0,118)+"..." : ""}
-              movieImage={`${image}${tvshow ? tvshow.poster_path : ""}`}
+              movieTitle={tvshow?.original_name || ""}
+              movieRelese={tvshow?.release_date || ""}
+              movieRating={tvshow?.vote_average || ""}
+              movieDescription={tvshow?.overview?.slice(0, 118) + "..." || ""}
+              movieImage={`${image}${tvshow?.poster_path || ""}`}
             />
           ))}
         </div>
+        <div className="button_container">
+          {currentPage < totalPages && (
+            <button onClick={handleLoadMore} className="load_more_button">
+              Load More
+            </button>
+          )}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
