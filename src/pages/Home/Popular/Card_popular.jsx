3e2b8b8eaa8ce4175cd041e530f5c309bbdc07e8
im4soft02 <ahmad.adhin.02@gmail.com>
2023-08-01 10:33:52 +0700

    Update?homePage

--- src/pages/Home/Popular/Card_popular.jsx
+++ src/pages/Home/Popular/Card_popular.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Component } from "react";
 import "../Popular/Card_popular.css";
 import { useParams } from "react-router-dom";
 import Card from "../../../components/Card/Card";
 import CardHome from "../../../components/CardHome/CardHome";
 import CardHomeNew from "../../../components/CardHome/CardHomeNew";
+// React Slider
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Card_popular = () => {
   const [movieList, setMovieList] = useState([]);
@@ -29,26 +33,82 @@ const Card_popular = () => {
 
   const imageBaseUrl = import.meta.env.VITE_APP_BASE_IMAGE_URL;
 
+  // React Slidet
+  const settings = {
+    useTransform: true,
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 8,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 2400,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1367,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="warp_popular">
       {/* Judl Card */}
-      <div className="Judul_warp">
-        <h1 className="text_judul">POPULAR</h1>
+      <div className="warp_judul_show">
+        {/* Judul */}
+        <div className="warp_hr_judul2">
+          <div className="Judul_warp_Up2">
+            <div className="text_judul2">POPULAR</div>
+            <div className="gradient2"></div>
+          </div>
+        </div>
+        {/* Show */}
+        <div className="warp_hr_judul3">
+          <div className="Judul_warp_Up3">
+            <div className="text_judul3">Show More</div>
+            {/* <div className="gradient3"></div> */}
+          </div>
+        </div>
       </div>
       {/* Card warp*/}
-      <div className="Card_warp">
+      {/* <div className="Card_warp"> */}
+      <Slider {...settings} className="">
         {movieList.map((movie, i) => (
-          <CardHomeNew
-            key={i}
-            movie={movie}
-            movieTitle={movie?.original_title || ""}
-            movieRelese={movie?.release_date || ""}
-            movieRating={movie?.vote_average || ""}
-            movieDescription={movie?.overview?.slice(0, 118) + "..." || ""}
-            movieImage={`${imageBaseUrl}${movie?.poster_path || ""}`}
-          />
+          <div className="Card_warp " key={i}>
+            <CardHomeNew
+              key={i}
+              movie={movie}
+              movieTitle={movie?.original_title || ""}
+              movieRelese={movie?.release_date || ""}
+              movieRating={movie?.vote_average || ""}
+              movieDescription={movie?.overview?.slice(0, 118) + "..." || ""}
+              movieImage={`${imageBaseUrl}${movie?.poster_path || ""}`}
+            />
+          </div>
         ))}
-      </div>
+      </Slider>
+      {/* </div> */}
     </div>
   );
 };
